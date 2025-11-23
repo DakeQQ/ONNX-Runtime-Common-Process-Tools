cf4027d7c42e39a482b1090557cf739e2706a01c
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-11-23 13:45:08 +0800

    Update Export_Stereo_to_Mono.py

--- Export_Stereo_to_Mono.py
+++ Export_Stereo_to_Mono.py
@@ -3,35 +3,34 @@ from onnxslim import slim
 
 
 save_path = "stereo_to_mono.onnx"
-DYNAMIC = True               # Set True for dunamic length input.
-INPUT_AUDIO_LENGTH = 16000   # For staic input shape.
+DYNAMIC = False              # Set True for dunamic length input.
+INPUT_AUDIO_LENGTH = 48000   # For staic input shape.
 IN_DTYPE = torch.int16       # [torch.int16, torch.float32]
 OUT_DTYPE = torch.int16      # [torch.int16, torch.float32]
-OPSET = 17
+OPSET = 23
 
 
 class StereoToMono(torch.nn.Module):
-    def __init__(self, input_length):
+    def __init__(self):
         super(StereoToMono, self).__init__()
-        self.inv_input_length = float(1.0 / input_length)
 
     def forward(self, stereo: torch.Tensor) -> torch.Tensor:
         stereo = stereo.view(-1, 2)
         if IN_DTYPE != torch.int16:
-            mono = stereo.mean(dim=1)
+            mono = stereo.mean(dim=-1, keepdim=False)
         else:
             stereo = stereo.float()
             if DYNAMIC:
-                mono = stereo.mean(dim=1)
+                mono = stereo.mean(dim=-1, keepdim=False)
             else:
-                mono = stereo.sum(dim=1)
-                mono *= self.inv_input_length
+                mono = stereo.sum(dim=-1, keepdim=False)
+                mono *= 0.5
         if OUT_DTYPE != torch.float32:
             mono = mono.to(torch.int16)
         return mono.view(1, 1, -1)
 
 
-model = StereoToMono(INPUT_AUDIO_LENGTH)
+model = StereoToMono()
 model.eval()
 
 dummy_input = torch.ones(INPUT_AUDIO_LENGTH + INPUT_AUDIO_LENGTH, dtype=IN_DTYPE)
@@ -66,4 +65,3 @@ slim(
 
 print(f"Model exported and optimized.")
 print(f"Exported ONNX model to {save_path}")
-
